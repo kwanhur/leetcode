8015260cc867144142066e17c6dbf1962af09a7d
kwanhur.huang <huang_hua2012@163.com>
2018-07-25 23:57:26 +0800

    problem: rotate two diversion array

---
+++ rotate-image.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix)
+        if not n:
+            return matrix
+
+        # rotate rows
+        for i, r in enumerate(matrix):
+            matrix[i] = r[::-1]
+
+        for i in range(n):
+            for j in range(n - i):
+                tr, tc = n - 1 - j, n - 1 - i
+                matrix[i][j], matrix[tr][tc] = matrix[tr][tc], matrix[i][j]
+
+
+if __name__ == '__main__':
+    so = Solution()
+
+
+    def test(m):
+        print 'origin:\n', m, '\nresult:\n'
+        so.rotate(m)
+        print m
+
+
+    l = [
+        [5, 1, 9, 11],
+        [2, 4, 8, 10],
+        [13, 3, 6, 7],
+        [15, 14, 12, 16]
+    ]
+    test(l)
+    l = [
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+    ]
+    test(l)
